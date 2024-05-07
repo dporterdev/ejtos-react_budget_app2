a4eaf281020a8d5b22fff17922c936b89246178e
David Porter <dporter.dev1@gmail.com>
2024-05-07 16:49:54 -0400

    fixed the total expeneses, need to fix budget updating

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,14 +2,27 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget,expenses } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
+
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total = total + item.cost);
+        }, 0);
+
+        if(newBudget < totalExpenses) {
+            alert("You cannot reduce the budget value lower than the spending");
+            setNewBudget("")
+            return;
+        }
+    
     }
+
+
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budget: £</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
